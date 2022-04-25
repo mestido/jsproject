666a130540f903deec737e72f9223c8734ae9fc8
Evgeny <mestidos@gmail.com>
2022-04-25 13:54:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+//alert("Hello World!");
+//const result = confirm("Are you sure?");
+//console.log(result);
+//const answer = +prompt("Вам есть 18?", "38");
+//console.log(answer + 5);
+//const answers = [];
+//answers[0] = prompt("Как ваше имя?", "");
+//answers[1] = prompt("Как ваше фамилия?", "");
+//answers[2] = prompt("Как вам луе?", "");
+//console.log(typeof answers);
+//console.log(typeof null);
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/5`);
+// const user = "John";
+// alert(`Hello, ${user}!`);
+// let incr = 10,
+//   decr = 10;
+// ++incr, --decr;
+// console.log(incr++);
+// console.log(decr--);
+// console.log(5 % 2);
+// console.log(2 + 2 * 2 !== "6");
+// const isChecked = true,
+//   isClose = false;
+// console.log(isChecked || !isClose);
